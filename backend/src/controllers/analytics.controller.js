5b2e13506823d981d3e2265525e3e8468d9615d9
adarshcode007 <adarsh19pandey@gmail.com>
2026-04-23 22:09:52 +0530

    Added more analytical inshights

--- backend/src/controllers/analytics.controller.js
+++ backend/src/controllers/analytics.controller.js
@@ -4,11 +4,51 @@ export const getAnalytics = async (req, res) => {
   try {
     const { code } = req.params;
 
+    // Total clicks
     const totalClicks = await Click.countDocuments({
       shortCode: code,
     });
 
-    res.json({ totalClicks });
+    // Referrer breakdown
+    const referrerStats = await Click.aggregate([
+      { $match: { shortCode: code } },
+      {
+        $group: {
+          _id: "$referrer",
+          count: { $sum: 1 },
+        },
+      },
+    ]);
+
+    // Device breakdown
+    const deviceStats = await Click.aggregate([
+      { $match: { shortCode: code } },
+      {
+        $group: {
+          _id: "$userAgent",
+          count: { $sum: 1 },
+        },
+      },
+    ]);
+
+    // Daily clicks
+    const dailyClicks = await Click.aggregate([
+      { $match: { shortCode: code } },
+      {
+        $group: {
+          _id: {
+            $dateToString: {
+              format: "%Y-%m-%d",
+              date: "$timestamp",
+            },
+          },
+          count: { $sum: 1 },
+        },
+      },
+      { $sort: { _id: 1 } },
+    ]);
+
+    res.json({ totalClicks, referrerStats, deviceStats, dailyClicks });
   } catch (err) {
     res.status(500).json({ message: "Analytics error" });
   }
